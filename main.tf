a7b7130e8e9f988e16067d9aca0e6d9acfd33304
abdulrehmanfaisal100 <arfaisal.ee41ceme@ee.ceme.edu.pk>
2023-08-23 18:58:10 +0500

    Formatting

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
-    required_providers {
+  required_providers {
     aws = {
-        source  = "hashicorp/aws"
-        version = "~> 4.16"
-        }
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
-    required_version = ">= 1.2.0"
+  }
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
