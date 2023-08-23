ddcb479d9c86d74d7038039a8af660e3f15853e3
abdulrehmanfaisal100 <arfaisal.ee41ceme@ee.ceme.edu.pk>
2023-08-23 19:54:29 +0500

    NewBucketName

--- main.tf
+++ main.tf
@@ -113,7 +113,7 @@ resource "aws_route_table_association" "My_VPC_association" {
 
 #create S3 bucket
 resource "aws_s3_bucket" "b" {
-  bucket = "abdulrehmanfaisal17954"
+  bucket = "checkassignment1389_2"
   acl    = "private"
 
   tags = {
