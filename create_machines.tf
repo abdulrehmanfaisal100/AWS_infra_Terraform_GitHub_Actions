b6d127e38578f9d385e78f0fbae4f6f41a97ce91
abdulrehmanfaisal100 <arfaisal.ee41ceme@ee.ceme.edu.pk>
2023-08-23 18:05:25 +0500

    Intial Commit

---
+++ create_machines.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "public_instance" {
+  ami                    = var.public_instance
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.My_VPC_Subnet_Public.id
+  vpc_security_group_ids = ["${aws_security_group.My_VPC_Security_Group_Public.id}"]
+  tags = {
+    Name = "public_instance"
+  }
+}
+
+resource "aws_instance" "private_instance" {
+  ami                    = var.private_instance
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.My_VPC_Subnet_Private.id
+  vpc_security_group_ids = ["${aws_security_group.My_VPC_Security_Group_Private.id}"]
+  tags = {
+    Name = "private_instance"
+  }
+}
