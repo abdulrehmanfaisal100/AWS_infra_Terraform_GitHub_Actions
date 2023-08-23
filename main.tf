aef104527164af97e2b059604295114b18a16905
abdulrehmanfaisal100 <arfaisal.ee41ceme@ee.ceme.edu.pk>
2023-08-23 19:58:16 +0500

    Minor Changes

--- main.tf
+++ main.tf
@@ -113,7 +113,7 @@ resource "aws_route_table_association" "My_VPC_association" {
 
 #create S3 bucket
 resource "aws_s3_bucket" "b" {
-  bucket = "checkassignment1389_2"
+  bucket = "check-assignment-abdrehui"
   acl    = "private"
 
   tags = {
