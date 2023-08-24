515c9ffd3c13c9f80ed56dc1e7d4426df42fd9c5
abdulrehmanfaisal100 <arfaisal.ee41ceme@ee.ceme.edu.pk>
2023-08-24 17:32:36 +0500

    bucket change

--- main.tf
+++ main.tf
@@ -113,7 +113,7 @@ resource "aws_route_table_association" "My_VPC_association" {
 
 #create S3 bucket
 resource "aws_s3_bucket" "b" {
-  bucket = "check-assignment-abdrehucy"
+  bucket = "check-assignment-abdrehucg"
   acl    = "private"
 
   tags = {
