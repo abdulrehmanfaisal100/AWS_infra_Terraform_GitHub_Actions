31c4e28b9617b4f8c8bfd70008bbf233ffc7cae0
abdulrehmanfaisal100 <arfaisal.ee41ceme@ee.ceme.edu.pk>
2023-08-25 19:17:39 +0500

    new

--- main.tf
+++ main.tf
@@ -24,28 +24,28 @@ terraform {
   }
 }
 
- resource "aws_s3_bucket" "b" {
-   bucket = "check-assignment-abdrehxcv"
-   acl    = "private"
-   versioning {
-     enabled = true
-   }
-   server_side_encryption_configuration {
-     rule {
-       apply_server_side_encryption_by_default {
-         sse_algorithm = "AES256"
-       }
+resource "aws_s3_bucket" "b" {
+  bucket = "check-assignment-abdrehxcv"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
     }
-   }
- }
- resource "aws_dynamodb_table" "statelock" {
-   name         = "verify"
-   billing_mode = "PAY_PER_REQUEST"
-   hash_key     = "LOCKID"
-   attribute {
-     name = "LOCKID"
-     type = "S"   
-    }
+  }
+}
+resource "aws_dynamodb_table" "statelock" {
+  name         = "verify"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LOCKID"
+  attribute {
+    name = "LOCKID"
+    type = "S"
+  }
 }
 
 
