84b0d7a236ca5fdadaf1ec237876cc7a7eb739a1
abdulrehmanfaisal100 <arfaisal.ee41ceme@ee.ceme.edu.pk>
2023-08-25 12:41:32 +0500

    Deleting state files

--- main.tf
+++ main.tf
@@ -1,29 +1,29 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
+terraform { #terraform block is used to specify configuration settings for the project
+  required_providers { #providers that terraform will use. Providers are an abstraction of API, responsible for exposing resources
+    aws = { #configuration of AWS plugin, can change name
+      source  = "hashicorp/aws" #source to download the plugin
+      version = "~> 4.16" #specify the version of plugin, it will pick the latest version in series of 4.x
     }
   }
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.2.0" #minimum version of terraform 
 }
 
-provider "aws" {
-  region  = var.region
-  profile = var.profile
-} # end provider
+provider "aws" { #configuring the aws provider
+  region  = var.region #terraform apply -var="profile=your_profile_name" (INSTEAD OF USING DEFAULT)
+  profile = var.profile #profile for authentication, I haven't specified default profile but the profile associated with access keys is typically set default
+}
 # create the VPC
-resource "aws_vpc" "My_VPC" {
+resource "aws_vpc" "My_VPC" { #aws_vpc is a keyword
   cidr_block           = var.vpcCIDRblock
-  instance_tenancy     = var.instanceTenancy
-  enable_dns_support   = var.dnsSupport
-  enable_dns_hostnames = var.dnsHostNames
+  # instance_tenancy     = var.instanceTenancy
+  # enable_dns_support   = var.dnsSupport
+  # enable_dns_hostnames = var.dnsHostNames
   tags = {
     Name = "My VPC"
   }
-} # end resource
+}
 # create the Subnet
-resource "aws_subnet" "My_VPC_Subnet_Public" {
+resource "aws_subnet" "My_VPC_Subnet_Public" { #
   vpc_id                  = aws_vpc.My_VPC.id
   cidr_block              = var.subnetCIDRblock
   map_public_ip_on_launch = var.mapPublicIP
@@ -31,7 +31,7 @@ resource "aws_subnet" "My_VPC_Subnet_Public" {
   tags = {
     Name = "My VPC Public Subnet"
   }
-} # end resource
+}
 resource "aws_subnet" "My_VPC_Subnet_Private" {
   vpc_id     = aws_vpc.My_VPC.id
   cidr_block = var.subnetCIDRblock1
@@ -45,17 +45,17 @@ resource "aws_subnet" "My_VPC_Subnet_Private" {
 
 # Create the Security Group
 resource "aws_security_group" "My_VPC_Security_Group_Private" {
-  vpc_id      = aws_vpc.My_VPC.id
+  vpc_id      = aws_vpc.My_VPC.id #Specifies the ID of the VPC that this security group belongs to. (AN ATTRIBUTE, CANNOT CHANGE NAME)
   name        = "My VPC Security Group Private"
   description = "My VPC Security Group Private"
   ingress {
-    security_groups = ["${aws_security_group.My_VPC_Security_Group_Public.id}"]
-    from_port       = 0
+    security_groups = ["${aws_security_group.My_VPC_Security_Group_Public.id}"]#traffic from public subnet is allowed
+    from_port       = 0 #0 means all ports
     to_port         = 0
-    protocol        = "-1"
+    protocol        = "-1" #all protocols are allowed
   }
   egress {
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]#"0.0.0.0/0" indicates that traffic can be sent to any destination IP address.
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -69,7 +69,7 @@ resource "aws_security_group" "My_VPC_Security_Group_Public" {
   name        = "My VPC Security Group Public"
   description = "My VPC Security Group Public"
   ingress {
-    cidr_blocks = ["${var.ingressCIDRblockPub}"]
+    cidr_blocks = ["${var.ingressCIDRblockPub}"]#only IPs allowed by the variable can access the vpc instances
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -86,38 +86,37 @@ resource "aws_security_group" "My_VPC_Security_Group_Public" {
 }
 
 # Create the Internet Gateway
-resource "aws_internet_gateway" "My_VPC_GW" {
+resource "aws_internet_gateway" "My_VPC_GW" { #safe to assign this way because we didn't give private instances a public IP.
   vpc_id = aws_vpc.My_VPC.id
   tags = {
     Name = "My VPC Internet Gateway"
   }
 } # end resource
 # Create the Route Table
-resource "aws_route_table" "My_VPC_route_table" {
+resource "aws_route_table" "My_VPC_route_table" {#Route table is for traffic within the vpc
   vpc_id = aws_vpc.My_VPC.id
-  tags = {
+  tags = {#Additionally, you'll need to associate your subnets with this route table. Each subnet should have an associated route table to define how traffic is routed within that subnet. You can use the aws_route_table_association resource to associate your subnets with the route table.
     Name = "My VPC Route Table"
   }
 } # end resource
 # Create the Internet Access
 resource "aws_route" "My_VPC_internet_access" {
   route_table_id         = aws_route_table.My_VPC_route_table.id
-  destination_cidr_block = var.destinationCIDRblock
+  destination_cidr_block = var.destinationCIDRblock#represents the IP addresses allowed to use the internet
   gateway_id             = aws_internet_gateway.My_VPC_GW.id
-} # end resource
+} 
 # Associate the Route Table with the Subnet
 resource "aws_route_table_association" "My_VPC_association" {
   subnet_id      = aws_subnet.My_VPC_Subnet_Public.id
   route_table_id = aws_route_table.My_VPC_route_table.id
-} # end resource
+}
 
 #create S3 bucket
 resource "aws_s3_bucket" "b" {
   bucket = "check-assignment-abdrehucz"
-  acl    = "private"
+  acl    = "private" #ACL stands for access control list and here it is specified that only the owner has access to it who made this bucket
 
   tags = {
     Name        = "My bucket"
-    Environment = "VPC_EndPoint_test"
   }
 }
--- variables.tf
+++ variables.tf
@@ -17,15 +17,15 @@ variable "profile" {
 variable "availabilityZone" {
   default = "us-east-1a"
 }
-variable "instanceTenancy" {
-  default = "default"
-}
-variable "dnsSupport" {
-  default = true
-}
-variable "dnsHostNames" {
-  default = true
-}
+# variable "instanceTenancy" {
+#   default = "default"
+# }
+# variable "dnsSupport" {
+#   default = true
+# }
+# variable "dnsHostNames" {
+#   default = true
+# }
 variable "vpcCIDRblock" {
   default = "10.0.0.0/16"
 }
